4469c7c98e8ebc61b6bfa104cbc28a543ddc4563
phantomspecial <win.2000@live.jp>
2020-09-25 04:14:28 +0900

    add-update-at fix-vuetify

--- app/javascript/components/rates.vue
+++ app/javascript/components/rates.vue
@@ -1,39 +1,42 @@
 <template>
   <div id="rates">
-    <h2>Rate and Values</h2>
+    <div class="page_info">
+      <h2>Rate and Values</h2>
+      <p>Last Update: {{ values.time | full_size_datetime }}</p>
+    </div>
     <v-simple-table id="rates_values_table" dense>
       <thead>
-        <tr>
-          <th>Currency</th>
-          <th>Rate</th>
-          <th>Stock</th>
-          <th>Value</th>
-        </tr>
+      <tr>
+        <th>Currency</th>
+        <th>Rate</th>
+        <th>Stock</th>
+        <th>Value</th>
+      </tr>
       </thead>
       <tbody>
-        <tr v-for="(rate, pair) in values.rates" v-bind:id="rate">
-          <td>{{ pair | pair_name }}</td>
-          <td>{{ rate }}</td>
-          <td>{{ get_stock(pair) }}</td>
-          <td>{{ current_value(rate, pair) | addComma }}</td>
-        </tr>
+      <tr v-for="(rate, pair) in values.rates" v-bind:id="rate">
+        <td>{{ pair | pair_name }}</td>
+        <td>{{ rate }}</td>
+        <td>{{ get_stock(pair) }}</td>
+        <td>{{ current_value(rate, pair) | addComma }}</td>
+      </tr>
       </tbody>
     </v-simple-table>
 
     <v-simple-table id="net_assets" dense>
       <tbody>
-        <tr>
-          <th>VC Total</th>
-          <td>{{ vc_total_yen | addComma }}</td>
-        </tr>
-        <tr>
-          <th>JPY</th>
-          <td>{{ jpy_stock | addComma }}</td>
-        </tr>
-        <tr>
-          <th>Net Assets</th>
-          <td>{{ get_net_assets() | addComma }}</td>
-        </tr>
+      <tr>
+        <th>VC Total</th>
+        <td>{{ vc_total_yen | addComma }}</td>
+      </tr>
+      <tr>
+        <th>JPY</th>
+        <td>{{ jpy_stock | addComma }}</td>
+      </tr>
+      <tr>
+        <th>Net Assets</th>
+        <td>{{ get_net_assets() | addComma }}</td>
+      </tr>
       </tbody>
     </v-simple-table>
   </div>
@@ -85,10 +88,12 @@ export default {
 
 <style scoped lang="scss">
   #rates {
-    padding-left: 10px;
-    padding-right: 10px;
-  }
-  #net_assets {
-    padding-top: 50px;
+    .page_info {
+      display: flex;
+      justify-content: space-between;
+    }
+    #net_assets {
+      padding-top: 50px;
+    }
   }
 </style>
--- app/javascript/filters/filters.js
+++ app/javascript/filters/filters.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import moment from "moment";
 
 Vue.filter('pair_name', function (pair_name) {
   return pair_name.replace('_jpy_rate', '').toUpperCase() ;
@@ -6,4 +7,12 @@ Vue.filter('pair_name', function (pair_name) {
 
 Vue.filter('addComma', function (value) {
   return value.toLocaleString();
+})
+
+Vue.filter('full_size_datetime', function (value) {
+  if (value === null) {
+    return 'Null'
+  } else {
+    return moment(value).format('YYYY/MM/DD hh:mm:ss')
+  }
 })
